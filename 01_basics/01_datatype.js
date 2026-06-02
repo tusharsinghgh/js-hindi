bf346fc8a1446069cc6f65b6cd5bc0b809fb2094
tushar singh <tusharsingh7947@gmail.com>
2026-06-02 07:00:18 +0000

    add files

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,7 @@
+let name = "Tushar"; //String
+let age = 20; //number
+let isGraduate = true; //boolean
+let state; //undefined
+let city = null; //null
+let bigNum = 434343434343434434343434; // bigint
+let id = Symbol("userId"); // symbol 
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 14565;
+let accountEmail = "tushar@gmail.com";
+var accountPass = "12565";
+accountCity = "Delhi";
+
+console.log([accountEmail,accountId,accountPass,accountCity]);
