81b76ca4541620209e46b049ce15f3dc477e9bf2
tushar singh <tusharsingh7947@gmail.com>
2026-06-08 15:34:18 +0000

    changes in array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,2 +1,42 @@
-let myArr = [0,2,4,5,6,8];
-console.log(myArr.length);
+// ARRAY METHODS
+
+// let myArr = [0,2,4,5,6,8];
+// console.log(myArr.length);
+
+// let myArr1 = [0,1,2,3,4,5];
+// myArr1.push(6);
+// console.log(myArr1);
+
+// let myArr2 = [12,23,34,45];
+// myArr2.pop();
+// console.log(myArr2);
+
+// let myArr3 = [1,2,3,4,5];
+// myArr3.unshift(10);
+// console.log(myArr3);
+
+// myArr3.shift();
+// console.log(myArr3);
+
+// const mynewArr = [21,32,43,54,55];
+// console.log(mynewArr.includes(10));
+
+// const mynewArr1 = mynewArr.join();
+// console.log(mynewArr1);
+
+
+
+
+// splice,slice
+
+const Arr = [1,2,3,4,5];
+const newArr = Arr.slice(1,3);
+console.log(newArr);
+
+
+const Arr1 = [1,2,3,4,5];
+const newArr1 = Arr1.splice(1,3);
+console.log(Arr1);
+console.log(newArr1);
+
+
