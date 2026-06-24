9a5847d7084c4e7f613998b2ea507930ff3d17dd
tushar singh <tusharsingh7947@gmail.com>
2026-06-24 12:32:20 +0000

    loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,37 @@
+
+const myArr = ['microsoft','adobe','photoshop']
+
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+    
+}
+
+for (let i = 0; i <=20; i++) {
+    // console.log(i);
+    for (let k = 0; k < 20; k++) {
+    //    console.log(k);
+       
+        
+    }
+    
+}
+
+for (let j = 0; j < 10; j++) {
+    if (j == 5) {
+        console.log(' 5 is detected');
+        break
+    }
+    console.log(j);
+    
+    
+}
+for (let j = 0; j < 10; j++) {
+    if (j == 5) {
+        console.log(' 5 is detected');
+        continue
+    }
+    console.log(j);
+    
+    
+}
