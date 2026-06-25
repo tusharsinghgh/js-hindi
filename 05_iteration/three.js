bcadafd272630a43f03395d67774e2972ab28763
tushar singh <tusharsingh7947@gmail.com>
2026-06-25 14:57:57 +0000

    add two and three

---
+++ 05_iteration/three.js
@@ -0,0 +1,26 @@
+// FOR OF
+
+let arr = [1,2,3,4,5];
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+let str = "Hello world!"
+
+for (const a of str) {
+//  console.log(a);
+    
+}
+
+let map = new Map()
+
+map.set('IN','India')
+map.set('US','United States')
+map.set('FR','France')
+
+for (const maps of map) {
+    console.log(maps);
+    
+}
---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+// WHILE LOOP
+
+// let i = 0;
+
+// while(i<=10){
+// console.log(i);
+// i++;
+// }
+
+// let myArr = ['flash','superman','batman'];
+// let arr = 0;
+
+// while (arr<myArr.length) {
+//     console.log(myArr[arr]);
+//     arr = arr+1
+// }
+
+// DO WHILE LOOP 
+
+let i = 15;
+ do {
+    console.log(i);
+    i++;
+    
+ } while (i<=10);
