b9e1ab9b7b3985b9b81d7c555afe59e8047fc24a
tushar singh <tusharsingh7947@gmail.com>
2026-07-18 06:22:30 +0000

    project 5

---
+++ 07_project/project5/p5.js
@@ -0,0 +1,19 @@
+const key = document.querySelector('#key')
+
+window.addEventListener('keydown',(e)=>{
+    key.innerHTML = `
+    <div>
+    <table>
+  <tr>
+    <th>key</th>
+    <th>keyCode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+</div>`
+})
