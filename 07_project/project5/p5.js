228bdc702b6b200b9bb157afc9672d4aede18f5d
tushar singh <tusharsingh7947@gmail.com>
2026-07-21 06:49:17 +0000

    xml

--- 07_project/project5/p5.js
+++ 07_project/project5/p5.js
@@ -10,7 +10,7 @@ window.addEventListener('keydown',(e)=>{
     <th>code</th>
   </tr>
   <tr>
-    <td>${e.key}</td>
+    <td>${e.key === " " ? 'space': e.key}</td>
     <td>${e.keyCode}</td>
     <td>${e.code}</td>
   </tr>
