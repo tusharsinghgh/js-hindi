cd50db2f9669ef34608bc62053d50abc19d1d17f
tushar singh <tusharsingh7947@gmail.com>
2026-07-24 08:17:45 +0000

    promise2

--- 09_Advance/promise.js
+++ 09_Advance/promise.js
@@ -1,36 +1,69 @@
-const promise1 = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        console.log('work done');
-        resolve()
-    },1000)
+// const promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log('work done');
+//         resolve()
+//     },1000)
+// })
+
+// promise1.then(()=>{
+// console.log('promise consumed');
+
+// })
+
+
+// new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//         console.log('work has been done');
+//         resolve();
+//     },1000)
+// }).then(()=>{
+//     console.log('done');
+
+// })
+
+// const promise3 = new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//         resolve({username:"@ur.tuxxhar",password:"tsh@122"})
+//     },1000)
+// })
+
+// promise3.then((user)=>{
+// console.log(user);
+// return user.password
+// }).then((data)=>{
+// console.log(data);
+
+// })
+
+const promise4 = new Promise((resolve, reject) => {
+    let err = false;
+    if (!err) {
+        resolve({ username: "Tushar", Pass: "198" })
+    } else {
+        reject('ERROR: Something went wrong')
+    }
 })
 
-promise1.then(()=>{
-console.log('promise consumed');
+promise4.then((data) => {
+    console.log(data);
 
+}).catch((error) => {
+    console.log(error);
 })
 
+const promise5 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let err = false;
+        if (!err) {
+            resolve({ username: "Tushar", Pass: "198" })
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
 
-new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        console.log('work has been done');
-        resolve();
-    },1000)
-}).then(()=>{
-    console.log('done');
+async function consumePromise5(
     
-})
-
-const promise3 = new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        resolve({username:"@ur.tuxxhar",password:"tsh@122"})
-    },1000)
-})
-
-promise3.then((user)=>{
-console.log(user);
-return user.password
-}).then((data)=>{
-console.log(data);
-
-})
+){
+    
+}
